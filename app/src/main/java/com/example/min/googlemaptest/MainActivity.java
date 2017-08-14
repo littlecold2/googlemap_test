c5f87dead54821c19ea06659d6e1376f14cc9473
Lee Min Haeng <littlecold2@naver.com>
2017-08-14 15:57:22 +0900

    urldownload ok

--- app/src/main/java/com/example/min/googlemaptest/MainActivity.java
+++ app/src/main/java/com/example/min/googlemaptest/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.min.googlemaptest;
 
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,12 +18,28 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
+
 //import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 
 
 
 public class MainActivity extends AppCompatActivity
         implements OnMapReadyCallback {
+
+    ArrayList<LatLng> MarkerPoints; // 나중에 쓰기
+    LatLng M1 = new LatLng(37.56, 126.97);
+    LatLng M2 = new LatLng(37.55, 126.96);
+   // Urltextview urlview;
+    String fetchedtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +50,16 @@ public class MainActivity extends AppCompatActivity
         MapFragment mapFragment = (MapFragment)fragmentManager
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        String url = getUrl(M1,M2);
+        fetchUrl fUrl = new fetchUrl();
+        fUrl.execute(url);
+        //fetchedtext = fetchUrl(url);
+
+        //Log.d("iii", "뭐지");
+      //  urlview = new Urltextview();
+     //   urlview = (Urltextview) getApplicationContext();  // Context를 해당 Class로 캐스팅해줘야 NullPointerException이 발생하지 않습니다.
+//        ((Urltextview)Urltextview.mContext).setStr("aaaaa");
+
 
     }
     private void pickMark(final GoogleMap map, final LatLng LL)
@@ -44,9 +75,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onMapReady(final GoogleMap map) {
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
-        LatLng GG = new LatLng(37.55, 126.96);
-        pickMark(map,GG);
+
+        pickMark(map,M2);
 /*        MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(SEOUL);
         markerOptions.title("서울");
@@ -58,7 +88,7 @@ public class MainActivity extends AppCompatActivity
         // 뭐지
 
         final Marker a = map.addMarker(new MarkerOptions()
-                .position(SEOUL)
+                .position(M1)
                 .draggable(true)
                 .snippet("한국의 수도")
                 .title("서울"));
@@ -85,6 +115,9 @@ public class MainActivity extends AppCompatActivity
                 return false;
             }
         });
+
+
+
         /*
      // 맵 위치를 이동하기
 		CameraUpdate update = CameraUpdateFactory.newLatLng(new LatLng(37.502985, 126.799043));
@@ -94,12 +127,124 @@ public class MainActivity extends AppCompatActivity
          */
         //
         //맵 위치로 이동하기
-        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        map.moveCamera(CameraUpdateFactory.newLatLng(M1));
         map.animateCamera(CameraUpdateFactory.zoomTo(12));
 
         map.setPadding(300,300,300,300); // left, top, right, bottom //버튼이나 그런거 위치 한정?
         map.getUiSettings().setZoomControlsEnabled(true);
        //  map.setMapType(GoogleMap.MAP_TYPE_HYBRID); // 지도 유형 변경
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new Button.OnClickListener()
+        { @Override
+            public void onClick(View view)
+            {
+                String url = getUrl(M1,M2);
+                fetchUrl fUrl = new fetchUrl();
+                fUrl.execute(url);
+               // fetchedtext = fUrl;
+                Toast.makeText(getApplicationContext(),"url 버튼 눌림",Toast.LENGTH_LONG).show();
+//                Intent MyIntent = new Intent(getApplicationContext(),Urltextview.class);
+//                Log.d("Ftext", url);
+//                MyIntent.putExtra("url",url);
+//                startActivity(MyIntent);
+                //EditText editTextName = (EditText) findViewById(R.id.editTextName) ;
+                // intent.putExtra("contact_name", editTextName.getText().toString()) ;
+            }
+        });
+
+
+
+
     }
+// https://maps.googleapis.com/maps/api/directions/outputFormat?parameters
+    //https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood4&key=YOUR_API_KEY
+    // origin=41.43206,-81.38992
+    private String getUrl(LatLng origin, LatLng dest)
+    {
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+//AIzaSyDIPfmJXw78A2tKbCtGZekNxAQcli7eoLM
+       //String url = "https://maps.googleapis.com/maps/api/directions/json?" +  str_origin +"&"+str_dest+"&key=AIzaSyDIPfmJXw78A2tKbCtGZekNxAQcli7eoLM";
+        String url = "https://maps.googleapis.com/maps/api/directions/json?origin=75+9th+Ave+New+York,+NY&destination=MetLife+Stadium+1+MetLife+Stadium+Dr+East+Rutherford,+NJ+07073&key=AIzaSyDIPfmJXw78A2tKbCtGZekNxAQcli7eoLM";
+        return url;
+    }
+    private String downloadUrl(String strUrl) throws IOException
+    {
+        String data = "";
+        InputStream iStream = null;
+        HttpsURLConnection urlConnection = null;
+        Log.d("Url",strUrl);
+        try{
+            URL url = new URL(strUrl);
+
+            // url 만들기
+            urlConnection = (HttpsURLConnection) url.openConnection();
+            Log.d("Url","1");
+
+            // 연결
+            urlConnection.connect();
+            Log.d("Url","2"); //여기서 안되네;;
+
+            // 데이터 읽기
+            iStream = urlConnection.getInputStream();
+            Log.d("Url","3");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+            Log.d("Url","4");
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while((line = br.readLine()) != null)
+            {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+            Log.d("downloadUrl", data.toString());
+            br.close();
+
+        }
+        catch (Exception e)
+        {
+            Log.d("Urlfail", "urldownloadfail");
+        }
+        finally
+        {
+            Log.d("Urlend", "end");
+            iStream.close();;
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+    private class fetchUrl extends AsyncTask<String, Void, String>
+
+    {
+        protected String doInBackground(String... url)
+        {
+            String data = "";
+            try {
+                data = downloadUrl(url[0]);
+                Log.d("dD", data.toString());
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+
+        }
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+
+            Intent MyIntent = new Intent(getApplicationContext(),Urltextview.class);
+            Log.d("Ftext", result);
+            MyIntent.putExtra("url",result);
+            startActivity(MyIntent);
+//            fetchedtext = result;
+        }
+    }//fetchUrl
+//    void urlbutton1(View v)
+//    {
+//    }
 
 }
+
---
+++ app/src/main/java/com/example/min/googlemaptest/Urltextview.java
@@ -0,0 +1,30 @@
+package com.example.min.googlemaptest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by MIN on 2017-08-08.
+ */
+
+public class Urltextview extends AppCompatActivity {
+   // public static Context mContext;
+    TextView tv;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.downloadview);
+        tv = (TextView) findViewById(R.id.urltext);
+        Intent intent = getIntent();
+        String uText = intent.getStringExtra("url");
+        tv.setText(uText);
+
+    }
+
+    public void setStr(String str)
+    {
+        tv.setText(str);
+    }
+}
